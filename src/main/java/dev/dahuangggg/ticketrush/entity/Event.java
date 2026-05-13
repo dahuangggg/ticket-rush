655bacf4e7ab3af0c035e7cfea40cc97fe333a51
dahuangggg <dahuangggg@icloud.com>
2026-05-14 02:53:14 +0800

    fix: use explicit Lombok imports and remove redundant @Mapper

--- src/main/java/dev/dahuangggg/ticketrush/entity/Event.java
+++ src/main/java/dev/dahuangggg/ticketrush/entity/Event.java
@@ -1,7 +1,10 @@
 package dev.dahuangggg.ticketrush.entity;
 
 import com.baomidou.mybatisplus.annotation.*;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
--- src/main/java/dev/dahuangggg/ticketrush/mapper/EventMapper.java
+++ src/main/java/dev/dahuangggg/ticketrush/mapper/EventMapper.java
@@ -2,7 +2,6 @@ package dev.dahuangggg.ticketrush.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import dev.dahuangggg.ticketrush.entity.Event;
-import org.apache.ibatis.annotations.Mapper;
 
 /**
  * 事件表 Mapper。
@@ -10,6 +9,5 @@ import org.apache.ibatis.annotations.Mapper;
  * 继承 BaseMapper 后，MyBatis-Plus 会提供常用 CRUD 方法：
  * selectById、selectOne、insert、updateById 等。
  */
-@Mapper
 public interface EventMapper extends BaseMapper<Event> {
 }
